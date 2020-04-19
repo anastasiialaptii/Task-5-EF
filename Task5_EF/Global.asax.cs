7b7bd0fcc86cb9cc0a27f23e316bd9d0b1a7f706
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-19 22:28:41 +0300

    add di for fluent validation

--- Task5_EF/Global.asax.cs
+++ Task5_EF/Global.asax.cs
@@ -9,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Task5_EF.Models;
 using Task5_EF.NinjectRegistration;
 
 
@@ -23,11 +24,21 @@ namespace Task5_EF
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            FluentValidationModelValidatorProvider.Configure();
+            //FluentValidationModelValidatorProvider.Configure();
+
+            ValidationConfiguration();
 
             NinjectModule registrations = new NinjectRegistrations();
             var kernel = new StandardKernel(registrations);
             DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+
+        private void ValidationConfiguration()
+        {
+            FluentValidationModelValidatorProvider.Configure(provider =>
+            {
+                provider.ValidatorFactory = new ValidatorFactory();
+            });
         }
     }
 }
--- Task5_EF/Models/FlowerModel.cs
+++ Task5_EF/Models/FlowerModel.cs
@@ -1,14 +1,14 @@
 ﻿using FluentValidation;
 using FluentValidation.Attributes;
+using System;
 using System.Collections.Generic;
 
 
 namespace Task5_EF.Models
 {
-    [Validator(typeof(FlowerValidator))]
+    //[Validator(typeof(FlowerValidator))]
     public class FlowerModel
-    {
-        
+    {       
         public int Id { get; set; }
 
         public string Name { get; set; }
@@ -26,6 +26,26 @@ namespace Task5_EF.Models
         public FlowerValidator()
         {
             RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+
+    public class ValidatorFactory : ValidatorFactoryBase
+    {
+        private static Dictionary<Type, IValidator> validators = new Dictionary<Type, IValidator>();
+
+        static ValidatorFactory()
+        {
+            validators.Add(typeof(IValidator<FlowerModel>), new FlowerValidator());
+        }
+
+        public override IValidator CreateInstance(Type validatorType)
+        {
+            IValidator validator;
+            if (validators.TryGetValue(validatorType, out validator))
+            {
+                return validator;
+            }
+            return validator;
         }
     }
 }
