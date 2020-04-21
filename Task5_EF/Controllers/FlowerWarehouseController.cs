7c87a606eb773877d84cfabdb1ca52a5dc13d6c4
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-22 01:07:51 +0300

    delete useless chunks of code

--- Task5_EF/Controllers/FlowerWarehouseController.cs
+++ Task5_EF/Controllers/FlowerWarehouseController.cs
@@ -13,25 +13,9 @@ namespace Task5_EF.Controllers
         public ActionResult GetFlowerWarehouseList()
         {
             SupplyContext supply = new SupplyContext();
-            //var query =
-            //   from post in database.Posts
-            //   join meta in database.Post_Metas on post.ID equals meta.Post_ID
-            //   where post.ID == id
-            //   select new { Post = post, Meta = meta };
 
-          //  return (from p in Context.Set<Product>()
-          //          where p.CategoryID == categoryID
-          //          select new { Name = p.Name }).ToList()
-          //.Select(x => new Product { Name = x.Name });
 
-           var res = (from flware in supply.FlowerWarehouses
-                        join fl in supply.Flowers
-                        on flware.FlowerId equals fl.Id
-                      
-                         select new { name = flware.FlowerModel}).ToList()
-                         .Select(x=>new FlowerWarehouseModel { FlowerModel = x.name });  
-                           
-
+           var res = supply.FlowerWarehouses.ToList();
 
             return View(res); ;
         }
